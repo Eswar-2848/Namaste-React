e4c0b2fe3e819a6dd689d588931694261bdd8b37
Eswar <tummalapallivaeswar@gmail.com>
2025-02-25 09:20:58 +0530

    feat():Making app production ready without using create-react-app

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", { key: "h1" }, "h1 tag"),
+    React.createElement("h2", { key: "h2" }, "h2 tag"),
+  ])
+);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
